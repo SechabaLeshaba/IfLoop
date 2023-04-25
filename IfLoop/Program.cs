43442fa71f11e528c7b37cfec77cf7a9b2cc9e1e
lab_services_student <lab_services_student@lab000000>
2023-04-25 14:26:45 +0000

    Add project files.

---
+++ IfLoop/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //declaration
+        int mark;
+        int sum = 0;
+        int average;
+
+        //inputs
+        for (int i = 0; i <= 2; i++)
+        {
+            Console.Write(" Enter mark {0}: ", (i + 1));
+            mark = Convert.ToInt32(Console.ReadLine());
+
+            if (mark < 0)
+            {
+                Console.WriteLine(" Wrong value entered,Please Re-Enter");
+                i = i - 1;
+            }
+            else if(mark > 100)
+            {
+                Console.WriteLine(" Wrong value entered,Please Re-Enter");
+                i = i - 1;
+            }
+            else
+            {
+                sum = sum + mark;
+            }
+            //Calculations
+            sum = sum + mark;
+        }
+       
+
+        //Calculations
+        
+        average= sum / 3;
+       
+
+        if(average < 50)
+        {
+            Console.WriteLine("fail");
+        }
+        else if(average < 75)
+        {
+            Console.WriteLine("Pass ");
+        }
+        else
+        {
+            Console.WriteLine("PD");
+        }
+        Console.WriteLine(average);
+    }
+}
